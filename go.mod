47c13d955ea6dd65db35582443b78e4540cdcb24
elias <593970471@qq.com>
2025-05-03 17:20:23 +0800

    encoding xxx -> utf8

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/miaoerwu/crawler
 
 go 1.24.2
+
+require (
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,42 +1,59 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
 func main() {
-	url := "https://www.thepaper.cn/"
-	resp, err := http.Get(url)
+	url := "https://www.chinanews.com.cn/"
+
+	body, err := Fetch(url)
 	if err != nil {
-		fmt.Println("fetch url error:", err)
+		fmt.Println("read content failed:", err)
 
 		return
+	}
+
+	fmt.Println(string(body))
+}
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
 	}
 	defer func(Body io.ReadCloser) {
 		_ = Body.Close()
 	}(resp.Body)
 
-	if resp.StatusCode != 200 {
+	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("Error status code: %v\n", resp.StatusCode)
 	}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("read response body error:", err)
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DetermineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
 
-		return
+	return io.ReadAll(utf8Reader)
+}
+
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	peek, err := r.Peek(1024)
+	if err != nil {
+		fmt.Println("fetch err:", err)
+
+		return unicode.UTF8
 	}
 
-	numsLinks := strings.Count(string(body), "<a")
-	fmt.Printf("homepage has: %d links!\n", numsLinks)
+	e, _, _ := charset.DetermineEncoding(peek, "")
 
-	exist := strings.Contains(string(body), "黄金")
-	fmt.Printf("是否存在黄金:%v\n", exist)
-
-	exist = bytes.Contains(body, []byte("黄金"))
-	fmt.Printf("是否存在黄金:%v\n", exist)
+	return e
 }
